ce87217afe67f524f095679685df1b1109e180ea
KaterynaKravchenko <ekaterina9968@gmail.com>
2016-08-21 22:33:42 +0300

    lesson 2

--- src/Class/TestName.java
+++ src/Class/TestName.java
@@ -2,5 +2,57 @@ package Class;
 
 public class TestName {
     public static void main(String[] args) {
-        System.out.println("Hello folks");    }
+      int a = 3, b = 10;
+        int result;
+        //  '%'
+        result = b%a;
+        System.out.println(result);
+        //  '/'
+        result = b/a;
+        System.out.println(result);
+        //  '++'
+        a += 10;
+        System.out.println(a);
+        //  '&&' 'if'
+        boolean test = false;
+        boolean test1 = true;
+          if(test && test1) {
+              System.out.println("inside if...");
+          }
+          else {
+              System.out.println("inside else...");
+          }
+       //  'for' + 'break'
+        boolean isActive = true;
+        for(int i = 5; i < 10; i++){
+            System.out.println("value " + i);
+            if(isActive)
+            break;
+        }
+        //  'for' + 'continue'
+        isActive = false;
+        for(int i = 5; i < 10; i++){
+            System.out.println("value " + i);
+            if(isActive)
+                continue;
+        }
+        //  'while'
+        int k = 10;
+        while (k > 0){
+            System.out.println(k);
+            k=-4;
+        }
+        //  'do while'
+        int c = 1;
+        do {
+            System.out.println("value is " + c);
+            c++;
+        } while (c <11);
+
+        int balance = 120;
+        if(balance > 100) {
+            System.out.println("finish");
+        }
+
+    }
 }
